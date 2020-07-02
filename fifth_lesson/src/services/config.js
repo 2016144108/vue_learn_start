8273f1e42a1ace42b487f1e2804728b2f30935bb
DESKTOP-AG1VF4J\86187 <2863279332@qq.com>
2020-07-02 11:44:09 +0800

    【vue第六节课】 1、获取新闻数据

---
+++ fifth_lesson/src/services/config.js
@@ -0,0 +1,1 @@
+export var APPCODE="a7cff6c1388d4b1aa1f682dad3527814";
---
+++ fifth_lesson/src/services/newService.js
@@ -0,0 +1,29 @@
+
+import axios from "axios";
+import { APPCODE } from "./config.js";
+
+export async function getNewsChannels(){//获取所有的新闻类别
+    //返回结果
+    const resp = await axios.get("http://ali-news.showapi.com/channelList", {//请求地址
+        headers: {
+            Authorization: `APPCODE ${APPCODE}`,//身份信息
+        },
+    });
+    return resp.data.showapi_res_body.channelList;
+}
+
+export async function getNews(channelId, page = 1, limit = 10){//获取所有的新闻信息
+    //返回结果
+    const resp = await axios.get("http://ali-news.showapi.com/newsList", {//请求地址
+        headers: {
+            Authorization: `APPCODE ${APPCODE}`,//身份信息
+        },
+        params:{
+            channelId,//类型id
+            page,//页码
+            maxResult: limit,//每页最大信息个数
+            needAllList: false,//关闭显示全部
+        }
+    });
+    return resp.data.showapi_res_body.pagebean.contentlist;
+}
