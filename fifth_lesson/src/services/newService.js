bc3127a419d6cfeb8b50a9e0a917a06b378e357b
DESKTOP-AG1VF4J\86187 <2863279332@qq.com>
2020-07-05 16:52:03 +0800

    【vue第十四课】 1、完成频道新闻页设计

--- fifth_lesson/src/services/newService.js
+++ fifth_lesson/src/services/newService.js
@@ -28,3 +28,20 @@ export async function getNews(channelId, page = 1, limit = 10){//获取所有的
     });
     return resp.data.showapi_res_body.pagebean.contentlist;
 }
+
+export async function getNewsList(channelId, page = 1, limit = 10){//获取所有的新闻信息
+    //返回结果
+    const resp = await axios.get("http://ali-news.showapi.com/newsList", {//请求地址
+        headers: {
+            Authorization: `APPCODE ${APPCODE}`,//身份信息
+        },
+        params:{
+            channelId,//类型id
+            page,//页码
+            maxResult: limit,//每页最大信息个数
+            needAllList: false,//关闭显示全部
+            needContent: 1,//需要内容
+        }
+    });
+    return resp.data.showapi_res_body.pagebean;
+}
--- fifth_lesson/src/views/ChannelNews.vue
+++ fifth_lesson/src/views/ChannelNews.vue
@@ -1,15 +1,24 @@
 <template>
     <div style="width: 1080px;margin: 70px auto 0;">
-        <NewsList :news="newsinfo"/>
-        <page style="margin: 0 auto" :page="page" :total="500" :limit="10" :paneNumber="10" @pagechange="handleChange"/>
+        <div class="title">
+            <h1 style="color: grey;">{{channelName}}</h1>
+            <hr>
+        </div>
+        <div v-show="isLoading" style="width: 1080px;margin: 20px auto 0;">
+            <Loading/>
+        </div>
+        <NewsList v-show="!isLoading" :news="newsinfo"/>
+        <page v-show="!isLoading" style="margin: 0 auto" :page="page" :total="total" :limit="limit" :paneNumber="paneNumber" @pagechange="handleChange"/>
     </div>
 </template>
 
 <script>
     // import CenterContainer from "../components/container/CenterContainer.vue"
-    import {getNews} from "../services/newService"
+    import {getNewsList} from "../services/newService"
+    import {getNewsChannels} from "../services/newService"
     import NewsList from "../components/news/NewsList"
     import Page from "../components/news/Page"
+    import Loading from "../components/Load/Loading"
 
     export default {
         name: "ChannelNews",
@@ -17,24 +26,68 @@
             /*CenterContainer,*/
             NewsList,
             Page,
+            Loading,
         },
         data() {
             return {
                 newsinfo: null,
-                page: 1,
+                channelName: null,
+                total: 500,
+                limit: 10,
+                paneNumber: 10,
+                isLoading: false,
+                /*page: 1,*/
             };
         },
-        async created() {
-            const route = this.$route;
-            this.newsinfo = await getNews(route.params.id);
-            console.log(this.newsinfo);
+        computed: {
+            page: {
+                get(){
+                    return +this.$route.query.page || 1;
+                },
+                set(){
+                }
+            },
         },
+        /*async created() {
+            this.setNews();
+            this.setNewsChannel();
+        },*/
         methods: {
             handleChange(newpage){
                 this.page = newpage;
-                console.log(this.page);
+                this.$router.push({//点击页码路由地址获取到相应的页码
+                    query: {
+                        page: newpage,
+                    },
+                });
+                this.setNews();
+            },
+            async setNewsChannel(){//设置频道
+                const channels =await getNewsChannels();
+                const channel = channels.find(
+                    (item) => item.channelId === this.$route.params.id
+                );
+                this.channelName = channel.name;
+            },
+            async setNews(){//设置新闻数据
+                this.isLoading = true;
+                const resp =await getNewsList(this.$route.params.id, this.page, this.limit);
+                this.total=resp.allNum;
+                this.newsinfo=resp.contentlist;
+                console.log(this.newsinfo);
+                this.isLoading = !this.isLoading;
             }
-        }
+        },
+        watch: {//监听id变化
+            "$route.params.id": {
+                immediate: true,//开始的变化也要监听到
+                handler(){//处理
+                    /*this.page = 1;*/
+                    this.setNewsChannel();
+                    this.setNews();
+                },
+            },
+        },
     }
 </script>
 
